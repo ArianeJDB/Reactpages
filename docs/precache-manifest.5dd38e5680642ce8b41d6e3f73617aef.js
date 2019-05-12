d6653c723f84d82d7433510a598654146379d2f3
arijdb <ariane.jurado@gmail.com>
2019-05-12 14:04:16 +0200

    pages

---
+++ docs/precache-manifest.5dd38e5680642ce8b41d6e3f73617aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe20d87feaa94391b7e345797605c30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75b9a163ebebe390f6fa",
+    "url": "./static/css/main.3154e883.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "./static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "75b9a163ebebe390f6fa",
+    "url": "./static/js/main.e78909eb.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
